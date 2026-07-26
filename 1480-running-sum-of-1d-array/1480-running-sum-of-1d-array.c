a04f14f713cab37e9debe2764011045cd90fa3d9
yavi603 <yavitiwari7@gmail.com>
2026-07-26 13:29:28 +0530

    Time: 0 ms (100%), Space: 11.7 MB (98.7%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.c
@@ -0,0 +1,14 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int* runningSum(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    result[0]=nums[0];
+    for(int i=1; i<numsSize; i++)
+    {
+        result[i]=result[i-1]+nums[i];
+    }
+    *returnSize = numsSize;
+    return result;
+}
